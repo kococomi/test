7070069ed064f16a257bfd9ae1e14cd205075e92
kococomi <hannie717@gmail.com>
2022-04-20 01:30:51 +0200

    Remove explore button from web UI

--- app/javascript/mastodon/features/explore/index.js
+++ app/javascript/mastodon/features/explore/index.js
@@ -49,15 +49,15 @@ class Explore extends React.PureComponent {
     const { intl, multiColumn, isSearching, layout } = this.props;
 
     return (
-      <Column bindToDocument={!multiColumn} ref={this.setRef} label={intl.formatMessage(messages.title)}>
+      <Column bindToDocument={!multiColumn} ref={this.setRef} label={intl.formatMessage(messages.searchResults)}>
         {layout === 'mobile' ? (
           <div className='explore__search-header'>
             <Search />
           </div>
         ) : (
           <ColumnHeader
-            icon={isSearching ? 'search' : 'hashtag'}
-            title={intl.formatMessage(isSearching ? messages.searchResults : messages.title)}
+            icon={'search'}
+            title={intl.formatMessage(messages.searchResults)}
             onClick={this.handleHeaderClick}
             multiColumn={multiColumn}
           />
@@ -68,19 +68,7 @@ class Explore extends React.PureComponent {
             <SearchResults />
           ) : (
             <React.Fragment>
-              <div className='account__section-headline'>
-                <NavLink exact to='/explore'><FormattedMessage id='explore.trending_statuses' defaultMessage='Posts' /></NavLink>
-                <NavLink exact to='/explore/tags'><FormattedMessage id='explore.trending_tags' defaultMessage='Hashtags' /></NavLink>
-                <NavLink exact to='/explore/links'><FormattedMessage id='explore.trending_links' defaultMessage='News' /></NavLink>
-                <NavLink exact to='/explore/suggestions'><FormattedMessage id='explore.suggested_follows' defaultMessage='For you' /></NavLink>
-              </div>
-
-              <Switch>
-                <Route path='/explore/tags' component={Tags} />
-                <Route path='/explore/links' component={Links} />
-                <Route path='/explore/suggestions' component={Suggestions} />
-                <Route exact path={['/explore', '/explore/posts', '/search']} component={Statuses} componentParams={{ multiColumn }} />
-              </Switch>
+              <div className='account__section-headline' />
             </React.Fragment>
           )}
         </div>
